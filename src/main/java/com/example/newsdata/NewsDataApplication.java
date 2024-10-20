8f99103ea7a6c1174c6e78baff3a78e9ec9f2170
yuuddin <hyj032564@gmail.com>
2024-10-20 10:36:49 +0900

    환경 세팅

---
+++ src/main/java/com/example/newsdata/NewsDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.newsdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/newsdata/NewsDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newsdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
